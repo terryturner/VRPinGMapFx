03127c5c52b8bd4d831b641ea8afc5234e91168c
peterpierce <peterpiercetw@gmail.com>
2017-05-30 23:32:19 +0800

    first commit

---
+++ GMapsFX-Examples/DirectionsExample/src/main/java/com/lynden/example/directions/DirectionsFXMLController.java
@@ -0,0 +1,81 @@
+package com.lynden.example.directions;
+
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.lynden.gmapsfx.service.directions.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+public class DirectionsFXMLController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
+
+    protected DirectionsService directionsService;
+    protected DirectionsPane directionsPane;
+
+    protected StringProperty from = new SimpleStringProperty();
+    protected StringProperty to = new SimpleStringProperty();
+    protected DirectionsRenderer directionsRenderer = null;
+
+    @FXML
+    protected GoogleMapView mapView;
+
+    @FXML
+    protected TextField fromTextField;
+
+    @FXML
+    protected TextField toTextField;
+
+    @FXML
+    private void toTextFieldAction(ActionEvent event) {
+        DirectionsRequest request = new DirectionsRequest(from.get(), to.get(), TravelModes.DRIVING);
+        directionsRenderer = new DirectionsRenderer(true, mapView.getMap(), directionsPane);
+        directionsService.getRoute(request, this, directionsRenderer);
+    }
+    
+    @FXML
+    private void clearDirections(ActionEvent event) {
+        directionsRenderer.clearDirections();
+    }
+    
+    @FXML
+    private void testAction(ActionEvent event) {
+    	mapView.setCenter(24.997861, 121.486786);
+    }
+
+    @Override
+    public void directionsReceived(DirectionsResult results, DirectionStatus status) {
+    	//System.out.println("direction got: " + results.getRoutes().get(0).getLegs().get(0).getDuration().getText());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        mapView.addMapInializedListener(this);
+        //mapView.setKey("AIzaSyAs71blnhxTVQj72XuGTgzkTIv5AqtDOlE");//terry
+        to.bindBidirectional(toTextField.textProperty());
+        from.bindBidirectional(fromTextField.textProperty());
+    }
+
+    @Override
+    public void mapInitialized() {
+        MapOptions options = new MapOptions();
+
+        options.center(new LatLong(24.997861, 121.486786))
+                .zoomControl(false)
+                .zoom(16)
+                .overviewMapControl(false)
+                .defaultUIControl(false)
+                .draggableControl(false)
+                .mapType(MapTypeIdEnum.ROADMAP);
+        GoogleMap map = mapView.createMap(options);
+        directionsService = new DirectionsService();
+        directionsPane = mapView.getDirec();
+    }
+
+}
