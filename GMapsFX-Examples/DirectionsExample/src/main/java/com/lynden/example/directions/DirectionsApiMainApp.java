c085813635892645a76b6696b0dc510b8635100c
Peter_Song <peter_song@goldtek.com>
2017-06-07 17:49:01 +0800

    Tsum Tsum

--- GMapsFX-Examples/DirectionsExample/src/main/java/com/lynden/example/directions/DirectionsApiMainApp.java
+++ GMapsFX-Examples/DirectionsExample/src/main/java/com/lynden/example/directions/DirectionsApiMainApp.java
@@ -8,15 +8,78 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import static javafx.application.Application.launch;
 
+//Peter+++
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+//Peter---
+
+
 
 public class DirectionsApiMainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/lynden/example/directions/Scene.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/com/lynden/example/directions/Scene.fxml"));
+//        
+//        Scene scene = new Scene(root);
+//        scene.getStylesheets().add("/styles/Styles.css");
         
-        Scene scene = new Scene(root);
+//        Peter+++
+
+    	Parent root = FXMLLoader.load(getClass().getResource("/com/lynden/example/directions/Scene.fxml"));
+        Scene scene = new Scene(root, 640, 480);
+        scene.setFill(Color.RED);
         scene.getStylesheets().add("/styles/Styles.css");
+
+        MenuBar menuBar = new MenuBar();
+        
+        // --- Menu File
+        Menu menuFile = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });
+        
+        menuFile.getItems().addAll(exit);
+ 
+        // --- Menu Edit
+        Menu menuEdit = new Menu("Edit");
+ 
+        // --- Menu View
+        Menu menuView = new Menu("View");
+ 
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+        
+        
+        if (scene.getRoot() instanceof VBox) {
+            ((VBox) scene.getRoot()).getChildren().addAll(menuBar);            	
+        } else if (scene.getRoot() instanceof AnchorPane) {
+            ((AnchorPane) scene.getRoot()).getChildren().addAll(menuBar);        	
+        }
+
+//        Peter---
+        
         
         stage.setTitle("Directions API Example");
         stage.setScene(scene);
@@ -34,5 +97,5 @@ public class DirectionsApiMainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    
 }
--- GMapsFX-Examples/DirectionsExample/src/main/java/com/lynden/example/directions/DirectionsFXMLController.java
+++ GMapsFX-Examples/DirectionsExample/src/main/java/com/lynden/example/directions/DirectionsFXMLController.java
@@ -17,6 +17,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
+//peter+++
+import java.util.Arrays;
+//peter---
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -27,7 +30,7 @@ import javafx.scene.control.TextField;
 
 public class DirectionsFXMLController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
 
-    protected DirectionsService directionsService;
+    	protected DirectionsService directionsService;
     protected DirectionsPane directionsPane;
 
     protected StringProperty from = new SimpleStringProperty();
@@ -36,12 +39,14 @@ public class DirectionsFXMLController implements Initializable, MapComponentInit
 
     @FXML
     protected GoogleMapView mapView;
-
-    @FXML
-    protected TextField fromTextField;
-
-    @FXML
-    protected TextField toTextField;
+    
+//    Peter+++
+//    @FXML
+//    protected TextField fromTextField;
+//
+//    @FXML
+//    protected TextField toTextField;
+//    Peter---
 
     @FXML
     private void toTextFieldAction(ActionEvent event) {
@@ -59,34 +64,35 @@ public class DirectionsFXMLController implements Initializable, MapComponentInit
     private void testAction(ActionEvent event) {
     	
     	mapView.setCenter(24.997861, 121.486786);
-        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyAs71blnhxTVQj72XuGTgzkTIv5AqtDOlE");
+        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCBczmGGGZSij3NsT3kACmZc7fbuKJ7yeI");
 //        context
 //        	.setConnectTimeout(1, TimeUnit.SECONDS)
 //        	.setReadTimeout(1, TimeUnit.SECONDS)
 //        	.setWriteTimeout(1, TimeUnit.SECONDS);
 
-        String[] origins = new String[]{
-                "沅聖科技股份有限公司", "萊爾富中和連勝店", "萊爾富中和力德店, 萊爾富北縣員山店",
-                "萊爾富北縣橋安店, 萊爾富北縣橋和二店"
-            };
-            String[] destinations = new String[]{
-            		"沅聖科技股份有限公司", "萊爾富中和連勝店", "萊爾富中和力德店, 萊爾富北縣員山店",
-                    "萊爾富北縣橋安店, 萊爾富北縣橋和二店"
-            };
-            try {
-				DistanceMatrix matrix =
-				    DistanceMatrixApi.getDistanceMatrix(context, origins, destinations).await();
-				
-				for (int i=0; i < matrix.originAddresses.length; i++) {
-					DistanceMatrixElement[] dm = matrix.rows[i].elements;
-					for (int j=0; j < dm.length; j++) {
-						System.out.println(String.format("%s to %s: %s", matrix.originAddresses[i], matrix.destinationAddresses[j], dm[j].duration.humanReadable));
-					}
-				}
-			} catch (ApiException | InterruptedException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+//        ++++++
+//        String[] origins = new String[]{
+//                "台中", "Goldtek"
+//            };
+//            String[] destinations = new String[]{
+//            		"台北", "Goldtek", "新北市中和區莒光路91號"
+//            };
+//            try {
+//				DistanceMatrix matrix =
+//				   DistanceMatrixApi.getDistanceMatrix(context, origins, destinations).await();
+//				
+//				for (int i=0; i < matrix.originAddresses.length; i++) {
+//					DistanceMatrixElement[] dm = matrix.rows[i].elements;
+//					for (int j=0; j < dm.length; j++) {
+//						System.out.println(String.format("%s to %s: %s --- %s", matrix.originAddresses[i], matrix.destinationAddresses[j], dm[j].duration.humanReadable, dm[j].distance.humanReadable));
+//					}
+//				}
+//			} catch (ApiException | InterruptedException | IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//            -------
+            
 //        GeocodingResult[] results;
 //		try {
 //			results = GeocodingApi.geocode(context,
@@ -97,6 +103,56 @@ public class DirectionsFXMLController implements Initializable, MapComponentInit
 //			e.printStackTrace();
 //		}
 //    	
+            //Peter ++ DirectionRequest
+            /*DirectionsRequest request = new DirectionsRequest("台中", "高雄", TravelModes.DRIVING);
+            directionsRenderer = new DirectionsRenderer(true, mapView.getMap(), directionsPane);
+            directionsService.getRoute(request, this, directionsRenderer); */
+            //-- DirectionRequest
+            //++ DirectionsWatpoint
+            //DirectionsWaypoint waypoint = new DirectionsWaypoint();
+	        String addressOrigin = "23.982196, 121.616099";
+	        String addressDestination = "南投";
+        	String[] O = new String[]{
+        			addressOrigin,
+            		"阿里山", 
+            		"陽明山", 
+            		"積穗國小", 
+            		"日月潭國家風景區", 
+            		"合歡山",
+            		addressDestination
+            		};
+            DirectionsWaypoint waypoint1 = new DirectionsWaypoint(O[1]);
+            DirectionsWaypoint waypoint2 = new DirectionsWaypoint(O[2]);
+            DirectionsWaypoint waypoint3 = new DirectionsWaypoint(O[3]);
+            DirectionsWaypoint waypoint4 = new DirectionsWaypoint(O[4]);
+            DirectionsWaypoint waypoint5 = new DirectionsWaypoint(O[5]);
+            DirectionsWaypoint[] test = { waypoint1, waypoint2, waypoint3, waypoint4, waypoint5};
+			DirectionsRequest request = new DirectionsRequest(addressOrigin, addressDestination, TravelModes.DRIVING, test);
+            directionsRenderer = new DirectionsRenderer(true, mapView.getMap(), directionsPane);
+            directionsService.getRoute(request, this, directionsRenderer); 
+            System.out.println("=============================");
+            try{
+            DistanceMatrix matrix =
+ 				   DistanceMatrixApi.getDistanceMatrix(context, O, O).await();
+            
+            for (int i=0; i < O.length; i++) {
+				DistanceMatrixElement[] dm = matrix.rows[i].elements;
+				int j=i+1;
+				System.out.println("j="+j);
+				if(j>=O.length){
+					j=0;
+					System.out.println("if="+j);
+				};
+//				for (int j=0; j < dm.length; j++) {
+				System.out.println(String.format("%s to %s: %s --- %s", matrix.originAddresses[i], matrix.destinationAddresses[j], dm[j].duration.humanReadable, dm[j].distance.humanReadable));
+//				}			
+				}
+            } catch (ApiException | InterruptedException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}           
+            //Peter-- DirectionsWatpoint
+            
     }
 
     @Override
@@ -108,8 +164,10 @@ public class DirectionsFXMLController implements Initializable, MapComponentInit
     public void initialize(URL url, ResourceBundle rb) {
         mapView.addMapInializedListener(this);
         //mapView.setKey("AIzaSyAs71blnhxTVQj72XuGTgzkTIv5AqtDOlE");//terry
-        to.bindBidirectional(toTextField.textProperty());
-        from.bindBidirectional(fromTextField.textProperty());
+//        Peter+++
+//        to.bindBidirectional(toTextField.textProperty());
+//        from.bindBidirectional(fromTextField.textProperty());
+//        Peter---
     }
 
     @Override
@@ -121,7 +179,7 @@ public class DirectionsFXMLController implements Initializable, MapComponentInit
                 .zoom(16)
                 .overviewMapControl(false)
                 .defaultUIControl(false)
-                .draggableControl(false)
+                .draggableControl(true)
                 .mapType(MapTypeIdEnum.ROADMAP);
         GoogleMap map = mapView.createMap(options);
         directionsService = new DirectionsService();
